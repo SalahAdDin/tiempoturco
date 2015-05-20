944cd7adaca495c42c894576cd3ad82625b0cd46
SalahAdDin <alagunajs@gmail.com>
2015-05-20 01:10:41 -0500

    Renombrados las direcciones a directorios rotas.

--- tiempoturco/settings/production.py
+++ tiempoturco/settings/production.py
@@ -81,6 +81,9 @@ EMAIL_SUBJECT_PREFIX = '[Tiempo Turco]'
 DEFAULT_FROM_EMAIL = 'oficial@tiempoturco.com'
 SERVER_EMAIL = 'oficial@tiempoturco.com'
 
+#Smart Selects
+USE_DJANGO_JQUERY = False
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
